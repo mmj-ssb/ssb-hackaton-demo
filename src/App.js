b22afbe5ae7764c06f8fb1a8cfa0940eb6174f65
mmj <mmj@ssb.no>
2018-09-25 16:09:15 +0200

    Double text.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
                 <Menu.Item>
                   <Link to='/analyzer'>
                     Tekstanalyse
-                    Datasetliste
                   </Link>
                 </Menu.Item>
 
